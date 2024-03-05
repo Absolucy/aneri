285155fd4b70a420bea09d954da22d93a33ae938
Lucy <lucy@absolucy.moe>
2024-03-05 18:33:29 -0500

    Hopefully better pick()?

--- crates/rand/src/global/pick.rs
+++ crates/rand/src/global/pick.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 use super::global;
-use meowtonin::ByondValue;
+use meowtonin::{ByondResult, ByondValue};
 use rand::{
 	distributions::{Distribution, WeightedIndex},
 	Rng,
@@ -8,13 +8,18 @@ use rand::{
 use std::collections::HashMap;
 
 #[byond_fn]
-pub fn pick(options: Vec<ByondValue>, secure: Option<bool>) -> Option<ByondValue> {
-	if options.is_empty() {
-		None
-	} else {
-		let length = options.len();
-		options.into_iter().nth(global(secure).gen_range(0..length))
+pub fn pick(options: ByondValue, secure: Option<bool>) -> ByondResult<Option<ByondValue>> {
+	if !options.is_list() {
+		return Ok(None);
 	}
+	let length = options.length()?;
+	match length {
+		0 => return Ok(None),
+		1 => return options.read_list_index(&1).map(Some),
+		_ => {}
+	}
+	let idx = global(secure).gen_range::<usize, _>(1..=length);
+	options.read_list_index(&idx).map(Some)
 }
 
 #[byond_fn]
@@ -23,13 +28,17 @@ pub fn pick_weighted(
 	secure: Option<bool>,
 ) -> Option<ByondValue> {
 	if options.is_empty() {
-		None
-	} else {
-		let weights = options.values().copied().collect::<Vec<f32>>();
-		let dist = WeightedIndex::new(weights).ok()?;
-		options
-			.into_iter()
-			.nth(dist.sample(&mut global(secure)))
-			.map(|(value, _)| value)
+		return None;
 	}
+	let (values, weights): (Vec<_>, Vec<_>) = options
+		.into_iter()
+		.filter(|(_, weight)| weight.is_normal() && weight.is_sign_positive())
+		.unzip();
+	match values.len() {
+		0 => return None,
+		1 => return values.into_iter().next(),
+		_ => {}
+	}
+	let dist = WeightedIndex::new(weights).ok()?;
+	values.into_iter().nth(dist.sample(&mut global(secure)))
 }
--- crates/rand/src/instance/pick.rs
+++ crates/rand/src/instance/pick.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 use super::instances;
 use aneri_core::ByondSlotKey;
-use meowtonin::ByondValue;
+use meowtonin::{ByondResult, ByondValue};
 use rand::{
 	distributions::{Distribution, WeightedIndex},
 	Rng,
@@ -9,15 +9,23 @@ use rand::{
 use std::collections::HashMap;
 
 #[byond_fn]
-pub fn instanced_pick(src: ByondSlotKey, options: Vec<ByondValue>) -> Option<ByondValue> {
-	if options.is_empty() {
-		None
-	} else {
-		let mut instances = instances().lock();
-		let rng = instances.get_mut(src)?;
-		let length = options.len();
-		options.into_iter().nth(rng.gen_range(0..length))
+pub fn instanced_pick(src: ByondSlotKey, options: ByondValue) -> ByondResult<Option<ByondValue>> {
+	if !options.is_list() {
+		return Ok(None);
 	}
+	let length = options.length::<usize>()?;
+	match length {
+		0 => return Ok(None),
+		1 => return options.read_list_index(&1).map(Some),
+		_ => {}
+	}
+	let mut instances = instances().lock();
+	let rng = match instances.get_mut(src) {
+		Some(rng) => rng,
+		None => return Ok(None),
+	};
+	let idx = rng.gen_range::<usize, _>(1..=length);
+	options.read_list_index(&idx).map(Some)
 }
 
 #[byond_fn]
@@ -26,15 +34,19 @@ pub fn instanced_pick_weighted(
 	options: HashMap<ByondValue, f32, ahash::RandomState>,
 ) -> Option<ByondValue> {
 	if options.is_empty() {
-		None
-	} else {
-		let mut instances = instances().lock();
-		let rng = instances.get_mut(src)?;
-		let (values, weights): (Vec<_>, Vec<_>) = options
-			.into_iter()
-			.filter(|(_, weight)| weight.is_normal() && weight.is_sign_positive())
-			.unzip();
-		let dist = WeightedIndex::new(weights).ok()?;
-		values.into_iter().nth(dist.sample(&mut *rng))
+		return None;
 	}
+	let (values, weights): (Vec<_>, Vec<_>) = options
+		.into_iter()
+		.filter(|(_, weight)| weight.is_normal() && weight.is_sign_positive())
+		.unzip();
+	match values.len() {
+		0 => return None,
+		1 => return values.into_iter().next(),
+		_ => {}
+	}
+	let mut instances = instances().lock();
+	let rng = instances.get_mut(src)?;
+	let dist = WeightedIndex::new(weights).ok()?;
+	values.into_iter().nth(dist.sample(&mut *rng))
 }
