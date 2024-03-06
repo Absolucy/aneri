466df7384d1824f3d410fa76bcad3a69a761456c
Lucy <lucy@absolucy.moe>
2024-03-05 19:38:21 -0500

    Move from static-init to once-cell for the core runtime

--- crates/core/src/runtime.rs
+++ crates/core/src/runtime.rs
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MPL-2.0
-use static_init::dynamic;
+use once_cell::sync::Lazy;
+use parking_lot::RwLock;
 use tokio::runtime::{Builder, Runtime};
 
-#[dynamic(drop)]
-pub static mut RUNTIME: Runtime = {
+pub static RUNTIME: Lazy<RwLock<Runtime>> = Lazy::new(|| {
 	Builder::new_multi_thread()
 		.enable_all()
 		.thread_name("meowtonin-tokio-runtime-worker")
 		.build()
+		.map(RwLock::new)
 		.expect("Failed to build Tokio runtime")
-};
+});
