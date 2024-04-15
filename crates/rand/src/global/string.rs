ef216c975d4048c38a16a39230f16118decd91da
Lucy <lucy@absolucy.moe>
2024-04-15 09:31:43 -0400

    Add new method, `replace_chars_prob`.

--- crates/rand/src/global/string.rs
+++ crates/rand/src/global/string.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
 use super::global;
-use rand::{distributions::Alphanumeric, Rng};
+use rand::{
+	distributions::{Alphanumeric, Bernoulli, Distribution},
+	Rng,
+};
 
 #[byond_fn]
 pub fn random_string_alphanumeric(length: usize, secure: Option<bool>) -> String {
@@ -8,4 +11,28 @@ pub fn random_string_alphanumeric(length: usize, secure: Option<bool>) -> String
 	(0..=length)
 		.map(|_| rng.sample(Alphanumeric) as char)
 		.collect()
+}
+
+#[byond_fn]
+pub fn replace_chars_prob(
+	input: String,
+	replacement: String,
+	prob: f32,
+	secure: Option<bool>,
+) -> String {
+	if prob <= 0.0 {
+		return input;
+	}
+	let mut rng = global(secure);
+	let distro =
+		Bernoulli::new((prob as f64 / 100.0).clamp(0.0, 1.0)).expect("invalid probability, wtf???");
+	let mut output = String::with_capacity(input.len() * replacement.len()); // Allocate for worst case scenario.
+	input.chars().for_each(|c| {
+		if distro.sample(&mut rng) {
+			output.push_str(&replacement);
+		} else {
+			output.push(c);
+		}
+	});
+	output
 }
--- crates/rand/src/instance/string.rs
+++ crates/rand/src/instance/string.rs
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 use super::INSTANCES;
 use aneri_core::ByondSlotKey;
-use rand::{distributions::Alphanumeric, Rng};
+use rand::{
+	distributions::{Alphanumeric, Bernoulli, Distribution},
+	Rng,
+};
 
 #[byond_fn]
 pub fn instanced_random_string_alphanumeric(src: ByondSlotKey, length: usize) -> Option<String> {
@@ -9,5 +12,30 @@ pub fn instanced_random_string_alphanumeric(src: ByondSlotKey, length: usize) ->
 		(0..=length)
 			.map(|_| rng.sample(Alphanumeric) as char)
 			.collect()
+	})
+}
+
+#[byond_fn]
+pub fn instnaced_replace_chars_prob(
+	src: ByondSlotKey,
+	input: String,
+	replacement: String,
+	prob: f32,
+) -> Option<String> {
+	if prob <= 0.0 {
+		return Some(input);
+	}
+	INSTANCES.lock().get_mut(src).map(|rng| {
+		let distro = Bernoulli::new((prob as f64 / 100.0).clamp(0.0, 1.0))
+			.expect("invalid probability, wtf???");
+		let mut output = String::with_capacity(input.len() * replacement.len()); // Allocate for worst case scenario.
+		input.chars().for_each(|c| {
+			if distro.sample(rng) {
+				output.push_str(&replacement);
+			} else {
+				output.push(c);
+			}
+		});
+		output
 	})
 }
