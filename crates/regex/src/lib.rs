499882b51253b725b9a8404176c262efb36bf556
Lucy <lucy@absolucy.moe>
2024-07-30 15:01:55 -0400

    Add `regex_combine_list`, also document `regex_escape`

--- crates/regex/src/lib.rs
+++ crates/regex/src/lib.rs
@@ -17,7 +17,22 @@ pub fn cleanup() {
 	instance::free_instances();
 }
 
+/// Escapes all regular expression meta characters in `pattern`.
+/// The string returned may be safely used as a literal in a regular expression.
 #[byond_fn]
 pub fn regex_escape(pattern: String) -> String {
 	regex::escape(&pattern)
+}
+
+/// Helper function to escape and combine a list of patterns to match.
+/// This is just so we only have to do one call_ext instead of doing it in a
+/// loop.
+/// The return value will use `|` as a separator.
+#[byond_fn]
+pub fn regex_combine_list(patterns: Vec<String>) -> String {
+	patterns
+		.into_iter()
+		.map(|pattern| regex::escape(&pattern))
+		.collect::<Vec<String>>()
+		.join("|")
 }
