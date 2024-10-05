094c7f10df51a29808c497fba2155b15a59fe7d0
Lucy <lucy@absolucy.moe>
2024-10-04 22:53:50 -0400

    oh uh forgot to push this

--- crates/time/src/timestamp.rs
+++ crates/time/src/timestamp.rs
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
-use std::time::{SystemTime, UNIX_EPOCH};
+use chrono::Local;
+use std::{
+	fmt::Write,
+	time::{SystemTime, UNIX_EPOCH},
+};
 
 #[byond_fn]
 pub fn unix_timestamp() -> String {
@@ -10,4 +14,17 @@ pub fn unix_timestamp() -> String {
 			.unwrap()
 			.as_secs_f64()
 	)
+}
+
+#[byond_fn]
+pub fn human_readable_timestamp(millis_precision: usize) -> String {
+	let now = Local::now();
+	let mut result = now.format("%Y-%m-%d %H:%M:%S").to_string();
+
+	if millis_precision > 0 {
+		let millis = now.timestamp_subsec_millis();
+		let _ = write!(result, ".{:0width$}", millis, width = millis_precision);
+	}
+
+	result
 }
