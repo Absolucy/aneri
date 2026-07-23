0098061724e743cfe574c940b78d45754eaed3e4
Lucy <lucy@absolucy.moe>
2026-07-23 18:42:46 -0400

    remove `aneri-experiments` and `aneri-pathfind`

--- crates/aneri/src/lib.rs
+++ crates/aneri/src/lib.rs
@@ -10,8 +10,6 @@ pub use aneri_crypto;
 pub use aneri_dmi;
 #[cfg(feature = "encode")]
 pub use aneri_encode;
-#[cfg(feature = "experiments")]
-pub use aneri_experiments;
 #[cfg(feature = "file")]
 pub use aneri_file;
 #[cfg(feature = "http")]
